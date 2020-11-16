a516612c2c494a7d2bc68213b0da0ea8e37efb4a
Matthew Cain <yooksi@zoho.com>
2020-11-16 18:31:21 +0100

    Add insert/remove battery recipes

---
+++ media/lua/server/wm_recipecode.lua
@@ -0,0 +1,35 @@
+----------------- Recipe TEST Functions -----------------
+
+--- Test if cassette player has no battery power left ---
+function Recipe_InsertBatteryIntoCassettePlayer_TestIsValid(sourceItem, result)
+
+	if sourceItem:getType() == "Walkman" then
+		return sourceItem:getUsedDelta() == 0;
+	else
+		return true; -- the battery
+	end
+end
+
+--- Test if cassette player has battery power left ---
+function Recipe_RemoveBatteryIntoCassettePlayer_TestIsValid(sourceItem, result)
+	return sourceItem:getUsedDelta() > 0;
+end
+
+------------------- Recipe Functions -------------------
+
+function Recipe_InsertBatteryIntoCassettePlayer(items, result, player)
+
+	local battery, cPlayer = items:get(0), items:get(1);
+
+	-- transfer battery power to cassette player
+	cPlayer:setUsedDelta(battery:getUsedDelta());
+end
+
+function Recipe_RemoveBatteryIntoCassettePlayer(items, result, player)
+
+	local cPlayer = items:get(0);
+
+	-- transfer power from cassette player to battery
+	result:setUsedDelta(cPlayer:getUsedDelta());
+	cPlayer:setUsedDelta(0);
+end
