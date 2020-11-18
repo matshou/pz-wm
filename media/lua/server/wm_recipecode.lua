9a43d322ea7b2213b3d2e7b861f423ace135edff
Matthew Cain <yooksi@zoho.com>
2020-11-18 01:18:27 +0100

    Add insert cassette recipe

--- media/lua/server/wm_recipecode.lua
+++ media/lua/server/wm_recipecode.lua
@@ -15,6 +15,16 @@ function Recipe_RemoveBatteryIntoCassettePlayer_TestIsValid(sourceItem, result)
 	return sourceItem:getUsedDelta() > 0;
 end
 
+--- Test if cassette player has no tape inserted ---
+function Recipe_InsertCassetteIntoCassettePlayer_TestIsValid(sourceItem, result)
+
+	if sourceItem:getType() == "Walkman" then
+		return not isCassetteInserted(sourceItem:getModData());
+	else
+		return true; -- the cassette
+	end
+end
+
 ------------------- Recipe Functions -------------------
 
 function Recipe_InsertBatteryIntoCassettePlayer(items, result, player)
@@ -32,6 +42,20 @@ function Recipe_RemoveBatteryIntoCassettePlayer(items, result, player)
 	-- transfer power from cassette player to battery
 	result:setUsedDelta(cPlayer:getUsedDelta());
 	cPlayer:setUsedDelta(0);
+end
+
+function Recipe_InsertCassetteIntoCassettePlayer(items, result, player)
+
+	local tape = items:get(0);
+	result:CopyModData(items:get(1):getModData());
+
+	local wm_data = result:getModData();
+	local tape_data = tape:hasModData() and tape:getModData() or InitCassetteItem(tape);
+
+	wm_data.tape_num = tape_data.num;
+	wm_data.track_num = tape_data.track;
+end
+
 function Recipe_RemoveCassetteFromCase(items, result, player)
 
 	player:getInventory():AddItem("WM.CassetteCaseEmpty");
